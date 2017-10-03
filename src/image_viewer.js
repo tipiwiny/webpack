0d1449f832432334a6552dc534a7acef797c68ad
Juan Luis Perez <tipiwiny@gmail.com>
2017-10-03 22:45:05 +0200

    config webpack in real React Project

--- src/image_viewer.js
+++
@@ -1,9 +0,0 @@
-import smaller from '../assets/smaller.jpeg'
-import '../styles/image_viewer.css';
-
-export default () => {
-  const smallImage = document.createElement('img');
-  smallImage.src = smaller;
-  document.body.appendChild(smallImage);
-
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,43 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-  entry: './src/index.js',
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+  'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: 'build/',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/,
+        exclude: /node_modules/
       },
       {
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader'
-        }),
-        test:/\.css$/
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { limit: 40000 }
-          },
-          'image-webpack-loader',
-        ]
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
   ]
 };
-
-module.exports = config;
